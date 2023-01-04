e47ed64341237ef22c34ad782a08312e4bcfb68d
rahulpandey70 <rahulpandey9103@gmail.com>
2023-01-04 11:39:04 +0530

    Modified admin controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,38 @@
-const getAllUsersController = () => {};
+const doctorModel = require("../models/doctorModel");
+const userModel = require("../models/userModel");
 
-const getAllDoctorsController = () => {};
+const getAllUsersController = async (req, res) => {
+	try {
+		const users = await userModel.find({});
+		res.status(200).send({
+			success: true,
+			message: "Users List Fetched successfully",
+			data: users,
+		});
+	} catch (error) {
+		res.status(500).send({
+			success: false,
+			message: "Error while fetching users list",
+			error,
+		});
+	}
+};
+
+const getAllDoctorsController = async (req, res) => {
+	try {
+		const doctors = await doctorModel.find({});
+		res.status(200).send({
+			success: true,
+			message: "Doctors List Fetched successfully",
+			data: doctors,
+		});
+	} catch (error) {
+		res.status(500).send({
+			success: false,
+			message: "Error while fetching doctors list",
+			error,
+		});
+	}
+};
 
 module.exports = { getAllUsersController, getAllDoctorsController };
